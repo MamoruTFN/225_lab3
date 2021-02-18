f04b2deb463325862f2f8e5911d66121c6999e8c
MamoruTFN <paweenwich.t@ku.th>
2021-02-18 15:09:50 +0700

    prime time

--- 6320502461_3_2.c
+++ 6320502461_3_2.c
@@ -2,6 +2,27 @@
 
 int main()
 {
+    unsigned int n;
+    scanf("%u",&n);
+    unsigned int a[n],b[n],max=0,p;
+    for (int i=0; i<n; i++)
+    {
+        scanf("%d %d",&a[i],&b[i]);
+    }
+    for (int i=0;i<n;i++)
+    {
+        if(a[i]==a[i+1])
+        {
+            b[i]+=b[i+1];
+        }
+        if(b[i]>max)
+        {
+            max=b[i];
+            p=i+1;
+        }
+    }
+    printf("%d %d",p,max);
+
 
 
     return 0;
