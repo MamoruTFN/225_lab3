fb00f69b5a637bbbb6399d8cc3a22f9b0b97920e
MamoruTFN <paweenwich.t@ku.th>
2021-02-18 15:25:22 +0700

    prime_t

--- 6320502461_3_2.c
+++ 6320502461_3_2.c
@@ -3,37 +3,37 @@
 int main()
 {
     int n,i;
-    scanf("%u",&n);
+    scanf("%d",&n);
     if(n>=1&&n<10000)
     {
-    int a[n],b[n],max=0,p=0;
+        int a[n],b[n],max=0,p=0;
 
-    for (i=0; i<n; i++)
-    {
-        scanf("%u %u",&a[i],&b[i]);
-    }
-    if(a[i]>=1&&a[i]<48&&b[i]>=1&&b[i]<50000)
-    {
         for (i=0; i<n; i++)
-    {
-        if(a[i]==a[i+1])
         {
-            b[i]+=b[i+1];
+            scanf("%d %d",&a[i],&b[i]);
         }
-        if(b[i]>max)
+
+
+        for (i=0; i<n; i++)
         {
-            max=b[i];
-            p=i+1;
+            if(a[i]>=1&&a[i]<48&&b[i]>=1&&b[i]<50000)
+            {
+                if(a[i]==a[i+1])
+                {
+                    b[i]+=b[i+1];
+                }
+                if(b[i]>max)
+                {
+                    max=b[i];
+                    p=i+1;
+                }
+            }
+
         }
+
+
+        printf("%d %d",p,max);
     }
-    }
-
-    printf("%u %u",p,max);
-    }
-
-
-
-
 
     return 0;
 }
