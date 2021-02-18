1c9743ee0d00c4d54695a337af8a804398d6aa39
MamoruTFN <paweenwich.t@ku.th>
2021-02-18 14:48:01 +0700

    pet

--- 6320502461_3_1.c
+++ 6320502461_3_1.c
@@ -2,7 +2,23 @@
 
 int main()
 {
+    int s,win=0,max=0,p;
+    for(int i=1;i<6;i++)
+    {
+        s=0;
+        for(int j=0;j<4;j++)
+        {
+            scanf("%d",&p);
+            s+=p;
+        }
+        if(s>max)
+        {
+            max=s;
+            win=i;
 
+        }
+    }
+    printf("%d %d",win,max);
 
     return 0;
 }
