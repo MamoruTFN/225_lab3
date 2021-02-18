16f982d824f64909b78f5c9da02967dbd95337fa
MamoruTFN <paweenwich.t@ku.th>
2021-02-18 17:11:30 +0700

    prime_f

--- 6320502461_3_4.c
+++ 6320502461_3_4.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 int main()
 {
-
-
+    int x,i=0,j;
+    scanf("%d" ,&x);
+    while(x>1)
+    {
+        if((x%2==0||x%3==0||x%5==0||x%7==0)&&(x!=2&&x!=3&&x!=5&&x!=7))
+        {
+            x--;
+        }
+        else
+        {
+            printf("%d" ,x);
+            break;
+        }
+    }
     return 0;
 }
