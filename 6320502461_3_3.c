561443747e6abea975248a301962590dcb69fedd
MamoruTFN <paweenwich.t@ku.th>
2021-02-18 15:59:24 +0700

    Factorization

--- 6320502461_3_3.c
+++ 6320502461_3_3.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 int main()
 {
-
+    int k;
+    scanf("%d",&k);
+    for (int i=2;k>1;)
+    {
+        if(k%i==0)
+        {
+            k=k/i;
+            printf("%d\n",i);
+        }
+        else
+            {
+                i++;
+            }
+    }
+    printf("0");
 
     return 0;
 }
